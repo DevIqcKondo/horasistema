919b2db4413411ec2103764ec060c4774126daba
Isadora Kondo <kondoiqcdev@gmail.com>
2021-05-21 18:41:08 -0300

    Execução do exercício

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace horasistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime horaatual = DateTime.Now;
+            Console.WriteLine(" Neste momento são exatamente "+horaatual);
+        }
+    }
+}
